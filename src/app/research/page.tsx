24a79e8c57a8556c1645b7114507d4f7bae72ab9
Hoang NT <me@gearons.org>
2025-02-05 16:12:09 +0900

    flip card for research page

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -1,6 +1,8 @@
 import TextFlip from "@/components/FlipText";
 import { SectionTitle } from "@/components/SectionTitle";
 import { translations } from "@/components/Dictionary";
+import IL11 from "@/public/img/il11.png"
+import FlipCard from "@/components/FlipCard";
 
 const text = translations.en;
 
@@ -15,6 +17,15 @@ export default function ResearchPage() {
       </SectionTitle>
       <div className="text-justify">
       <TextFlip />
+      </div>
+      <div>
+        <FlipCard
+          description="Computer programming or coding is the composition of sequences of instructions, called programs, that computers can follow to perform tasks."
+          image={IL11}
+          rotate="y"
+          subtitle="Stuffs"
+          title="Healthy Aging"
+        />
       </div>
     </div>
   )
