67fba989e38a662725d49fa478b02096ea20a4b5
Sotatek-VuongLe <vuong.le@sotatek.com>
2025-06-10 10:25:51 +0700

    feat: update select heart rate

--- src/app/shi-dashboard/components/HeartBeatRateChart.tsx
+++ src/app/shi-dashboard/components/HeartBeatRateChart.tsx
@@ -4,9 +4,7 @@ import React, { useEffect, useRef } from "react";
 import * as vg from "@uwdata/vgplot";
 import { withClientSideRendering } from "@/components/hoc";
 import StatsBox from "./StatsBox";
-
-// Predefined colors for lines (wraps if more subjects than colors)
-const colors = ["#60a5fa", "#facc15"];
+import { cn } from "@/libs/utils";
 
 interface HeartBeatRateChartProps {
   /*
@@ -18,7 +16,11 @@ interface HeartBeatRateChartProps {
    *  001,00:03,103
    *  001,00:04,104
    */
-  csvPaths: string[];
+  csvPaths: {
+    path: string;
+    color: string;
+    label: string;
+  }[];
   width?: number;
   height?: number;
   text?: string;
@@ -32,11 +34,17 @@ const HeartBeatRateChart: React.FC<HeartBeatRateChartProps> = ({
 }) => {
   const chartRef = useRef<HTMLDivElement>(null);
   const chartZoomRef = useRef<HTMLDivElement>(null);
+  const [loading, setLoading] = React.useState(true);
 
   useEffect(() => {
     const container = chartRef.current;
     const containerZoomed = chartZoomRef.current;
     if (!container || !containerZoomed) return;
+
+    if (!csvPaths.length) {
+      setLoading(false);
+      return;
+    }
 
     let initialized = false;
 
@@ -45,12 +53,12 @@ const HeartBeatRateChart: React.FC<HeartBeatRateChartProps> = ({
       await coord.databaseConnector(vg.wasmConnector());
 
       // Derive subject metadata from csvPaths
-      const subjects = csvPaths.map((path, index) => {
+      const subjects = csvPaths.map(({ color, path, label }, index) => {
         // Extract numeric id from filename: subject_001.csv
         const rawTable = `heart_rate_raw_${index}`;
         const endpointTable = `endpoint_${index}`;
         const dataUrl = `${window.location.origin}${path}`;
-        return { rawTable, endpointTable, dataUrl };
+        return { rawTable, endpointTable, dataUrl, color, label };
       });
 
       // Build exec commands dynamically
@@ -92,9 +100,7 @@ const HeartBeatRateChart: React.FC<HeartBeatRateChartProps> = ({
       // Build plot layers
       const layers: any[] = [];
       const layersText: any[] = [];
-      subjects.forEach(({ rawTable }, i) => {
-        const color = colors[i % colors.length];
-
+      subjects.forEach(({ rawTable, color }, i) => {
         // Line layer
         layers.push(
           vg.lineY(vg.from(rawTable), {
@@ -107,15 +113,13 @@ const HeartBeatRateChart: React.FC<HeartBeatRateChartProps> = ({
         );
       });
 
-      subjects.forEach(({ endpointTable }, i) => {
-        const color = colors[i % colors.length];
-
+      subjects.forEach(({ endpointTable, color, label }, i) => {
         // Endpoint text layer
         layersText.push(
           vg.text(vg.from(endpointTable), {
             x: "time_hours",
             y: "hr_min",
-            text: [`${text} ${i + 1}`],
+            text: [label],
             fill: color,
             lineAnchor: "bottom",
             dy: -6,
@@ -158,6 +162,8 @@ const HeartBeatRateChart: React.FC<HeartBeatRateChartProps> = ({
         containerZoomed.appendChild(chartZoomed as unknown as HTMLElement);
         initialized = true;
       }
+
+      setLoading(false);
     })().catch(console.error);
 
     // Cleanup on unmount
@@ -167,70 +173,96 @@ const HeartBeatRateChart: React.FC<HeartBeatRateChartProps> = ({
     };
   }, []);
 
+  const noData = csvPaths.length === 0;
+
   return (
-    <div className="flex flex-col gap-4">
-      <div className="flex gap-4">
-        <div className="flex flex-col gap-4 grow">
-          <StatsBox
-            label="30-year fixed-rate"
-            rate="7.22%"
-            rows={[
-              {
-                label: "1-week change",
-                rate: "+0.05%",
-              },
-              {
-                label: "1-year change",
-                rate: "+0.83%",
-              },
-              {
-                label: "4-week average",
-                rate: "7.09%",
-              },
-              {
-                label: "52-week average",
-                rate: "6.97%",
-              },
-            ]}
-            color="blue"
-          />
-
-          <StatsBox
-            label="15-year fixed-rate"
-            rate="6.47%"
-            rows={[
-              {
-                label: "1-week change",
-                rate: "+0.03%",
-              },
-              {
-                label: "1-year change",
-                rate: "+0.71%",
-              },
-              {
-                label: "4-week average",
-                rate: "6.37%",
-              },
-              {
-                label: "52-week average",
-                rate: "6.29%",
-              },
-            ]}
-            color="yellow"
-          />
-        </div>
-
-        <div className="bg-slate-100 rounded-md p-6 w-fit">
-          <h2 className="text-start mb-2">Heart Rate Zoomed</h2>
-          <div ref={chartZoomRef} className="w-fit" />
-        </div>
+    <>
+      <div
+        className={cn(
+          "flex items-center h-[400px] justify-center",
+          !loading && "hidden"
+        )}
+      >
+        <span className="text-gray-500">Loading...</span>
       </div>
 
-      <div className="bg-slate-100 rounded-md p-6">
-        <h2 className="text-start mb-2">Heart Rate Data</h2>
-        <div ref={chartRef} />
+      <div
+        className={cn(
+          "flex items-center h-[400px] justify-center",
+          (loading || !noData) && "hidden"
+        )}
+      >
+        <span className="text-gray-500">
+          You have not selected any data yet
+        </span>
       </div>
-    </div>
+
+      <div
+        className={cn("flex flex-col gap-4", (loading || noData) && "hidden")}
+      >
+        <div className="flex gap-4">
+          <div className="flex flex-col gap-4 grow">
+            <StatsBox
+              label="30-year fixed-rate"
+              rate="7.22%"
+              rows={[
+                {
+                  label: "1-week change",
+                  rate: "+0.05%",
+                },
+                {
+                  label: "1-year change",
+                  rate: "+0.83%",
+                },
+                {
+                  label: "4-week average",
+                  rate: "7.09%",
+                },
+                {
+                  label: "52-week average",
+                  rate: "6.97%",
+                },
+              ]}
+              color="blue"
+            />
+
+            <StatsBox
+              label="15-year fixed-rate"
+              rate="6.47%"
+              rows={[
+                {
+                  label: "1-week change",
+                  rate: "+0.03%",
+                },
+                {
+                  label: "1-year change",
+                  rate: "+0.71%",
+                },
+                {
+                  label: "4-week average",
+                  rate: "6.37%",
+                },
+                {
+                  label: "52-week average",
+                  rate: "6.29%",
+                },
+              ]}
+              color="yellow"
+            />
+          </div>
+
+          <div className="bg-slate-100 rounded-md p-6 w-fit">
+            <h2 className="text-start mb-2">Heart Rate Zoomed</h2>
+            <div ref={chartZoomRef} className="w-fit" />
+          </div>
+        </div>
+
+        <div className="bg-slate-100 rounded-md p-6">
+          <h2 className="text-start mb-2">Heart Rate Data</h2>
+          <div ref={chartRef} />
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/app/shi-dashboard/page.tsx
+++ src/app/shi-dashboard/page.tsx
@@ -1,12 +1,45 @@
+"use client";
+
 import HeartBeatRateChart from "@/app/shi-dashboard/components/HeartBeatRateChart";
+import { cn } from "@/libs/utils";
+import {
+  Description,
+  Field,
+  Label,
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions
+} from "@headlessui/react";
+import { CheckIcon, ChevronDownIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 const csvPaths = [
-  "/data/subject_001.csv",
-  "/data/subject_002.csv",
-  "/data/subject_003.csv",
+  {
+    path: "/data/subject_001.csv",
+    color: "#60a5fa",
+  },
+  {
+    path: "/data/subject_002.csv",
+    color: "#facc15",
+  },
+  {
+    path: "/data/subject_003.csv",
+    color: "#8B4513",
+  },
 ];
 
+const datasets = csvPaths.map((item, index) => ({
+  color: item.color,
+  path: item.path,
+  id: item.path,
+  label: `Heart rate ${index + 1}`,
+}));
+
 export default function SHIDashboard() {
+  const [selectedDataset, setSelectedDataset] = useState([...datasets]);
+  console.log("🚀 ~ SHIDashboard ~ selectedDataset:", selectedDataset)
+
   return (
     <div className="flex flex-col px-6 max-w-screen-xl mx-auto gap-4">
       <h1 className="text-5xl font-bold">SHI Summer School July 2025</h1>
@@ -14,12 +47,69 @@ export default function SHIDashboard() {
       <h2 className="text-3xl font-bold">Daily Heart Rate Patterns</h2>
 
       <span className="text-sm">
-        There are 
-        <span className="border-b-2 border-b-blue-400"> 15 interactive graphs</span>, showing the relationship between sleep and other lifestyle aspects. These data is synthesized by sampling from a real data distribution, which consists of 16290 person-days of minute-by-minute recordings.
+        There are
+        <span className="border-b-2 border-b-blue-400">
+          {" "}
+          15 interactive graphs
+        </span>
+        , showing the relationship between sleep and other lifestyle aspects.
+        These data is synthesized by sampling from a real data distribution,
+        which consists of 16290 person-days of minute-by-minute recordings.
       </span>
 
+      <Field>
+        <Label>Select datasets</Label>
+        <Description className={"mb-2 text-xs text-gray-500"}>
+          Only datasets with checked data will be shown in the chart.
+        </Description>
+
+        <Listbox value={selectedDataset} onChange={setSelectedDataset} multiple>
+          <ListboxButton
+            className={cn(
+              "relative block w-[200px] max-w-[200px] h-[36px] text-ellipsis whitespace-nowrap overflow-hidden rounded-lg bg-gray-200 py-1.5 pr-8 pl-3 text-left text-sm/6",
+              "focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25"
+            )}
+          >
+            {selectedDataset.map((person) => person.label).join(", ")}
+
+            <ChevronDownIcon
+              className="group pointer-events-none absolute top-2.5 right-2.5 size-4"
+              aria-hidden="true"
+            />
+          </ListboxButton>
+          <ListboxOptions
+            anchor="bottom"
+            transition
+            className={cn(
+              "w-[200px] rounded-xl border bg-gray-100 p-1 mt-1 focus:outline-none",
+              "transition duration-100 ease-in data-leave:data-closed:opacity-0"
+            )}
+          >
+            {datasets.map((data) => {
+              const isSelected = selectedDataset.some(
+                (item) => item.id === data.id
+              );
+              return (
+                <ListboxOption
+                  className="group text-sm flex justify-between cursor-default items-center gap-2 rounded-lg px-3 py-1.5 select-none data-focus:bg-gray-400"
+                  key={data.id}
+                  value={data}
+                >
+                  {data.label}
+
+                  <CheckIcon
+                    className={cn("invisible size-4", isSelected && "visible")}
+                  />
+                </ListboxOption>
+              );
+            })}
+          </ListboxOptions>
+        </Listbox>
+      </Field>
+
       <HeartBeatRateChart
-        csvPaths={csvPaths}
+        key={selectedDataset.map((d) => d.id).join(",")}
+        csvPaths={selectedDataset}
         width={1184}
         height={500}
         text="Heart Rate"
