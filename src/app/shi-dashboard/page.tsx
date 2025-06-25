483a81edb4b3757c7ee91d9421eaba93ce11f9c5
Hoang NT <me@gearons.org>
2025-06-25 11:53:33 +0900

    placeholders

--- src/app/shi-dashboard/page.tsx
+++ src/app/shi-dashboard/page.tsx
@@ -206,6 +206,42 @@ export default function SHIDashboard() {
 
       <h2 className="text-3xl font-bold mt-8">Sleep Architecture: Light/REM/Deep</h2>
       
+      <h3 className="text-2xl font-bold">Caffeine Consumption / Deep Sleep</h3>
+      <span className="text-sm">
+        <ul className="list-disc ml-5">
+          <li>Caffeine Consumption: Cups per day</li>
+          <li>Deep Sleep Time: Minute</li>
+        </ul>
+      </span>
+
+      <PlotColsGroup
+        fields={[
+          {
+            color: "black",
+            label: "Caffeine Consumption",
+            column: "start_hour",
+            xlabel: "Amount per day (cup)",
+            ylabel: "Number of records",
+          },
+          {
+            color: "darkblue",
+            label: "Deep Sleep Time",
+            column: "FB_minutesasleep_stages",
+            xlabel: "Deep Sleep Time (minutes)",
+            ylabel: "Number of records",
+          },
+        ]}
+        filePath="/data/fitbit_main_sleep.parquet"
+      />
+
+
+      <h3 className="text-2xl font-bold">Caffeine Consumption / REM Sleep</h3>
+      <span className="text-sm">
+        <ul className="list-disc ml-5">
+          <li>Caffeine Consumption: Cups per day</li>
+          <li>Sleep Start Time: 24-hour</li>
+        </ul>
+      </span>
 
       <PlotColsGroup
         fields={[
@@ -226,7 +262,6 @@ export default function SHIDashboard() {
         ]}
         filePath="/data/fitbit_main_sleep.parquet"
       />
-
 
     </div>
   );
