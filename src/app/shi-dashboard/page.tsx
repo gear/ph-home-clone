f3c682b02e0f3ca25b34d39dabe03a1cdd1d5d29
Hoang NT <me@gearons.org>
2025-07-11 22:48:02 +0900

    add activity

--- src/app/shi-dashboard/page.tsx
+++ src/app/shi-dashboard/page.tsx
@@ -422,7 +422,182 @@ export default function SHIDashboard() {
       */}
 
       <h2 className="text-3xl font-bold mt-8">3. Activity Effect</h2>
+      
 
+
+      <h3 className="text-2xl font-bold">Steps per Day / Sleep Efficiency (ranges 0 to 1)</h3>
+      <span className="text-sm">
+        <ul className="list-disc ml-5">
+          <li>Steps per Day: Steps count per day</li>
+          <li>Sleep Efficiency: Fraction from 0 to 1</li>
+        </ul>
+      </span>
+      <PlotColsGroup
+        fields={[
+          {
+            color: "lightslategrey",
+            label: "Steps per Day",
+            column: "Steps_per_day",
+            xlabel: "Steps per Day (Count)",
+            ylabel: "Number of records",
+          },
+          {
+            color: "teal",
+            label: "Sleep Efficiency (in fraction)",
+            column: "Sleep_efficiency",
+            xlabel: "Sleep Efficiency",
+            ylabel: "Number of records",
+          },
+        ]}
+        filePath="/data/activity_sleep.parquet"
+        parquetName="activity"
+      />
+
+      <h3 className="text-2xl font-bold">Steps per Day / Total Sleep Time</h3>
+      <span className="text-sm">
+        <ul className="list-disc ml-5">
+          <li>Steps per Day: Steps count per day</li>
+          <li>Total Sleep Time: Hours</li>
+        </ul>
+      </span>
+      <PlotColsGroup
+        fields={[
+          {
+            color: "lightslategrey",
+            label: "Steps per Day",
+            column: "Steps_per_day",
+            xlabel: "Steps per Day (count)",
+            ylabel: "Number of records",
+          },
+          {
+            color: "darkblue",
+            label: "Total Sleep Time (in hour)",
+            column: "Sleep_duration_h",
+            xlabel: "Total Sleep Time",
+            ylabel: "Number of records",
+          },
+        ]}
+        filePath="/data/activity_sleep.parquet"
+        parquetName="activity"
+      />
+
+
+
+      <h3 className="text-2xl font-bold">Body Mass Index / Sleep Efficiency (ranges 0 to 1)</h3>
+      <span className="text-sm">
+        <ul className="list-disc ml-5">
+          <li>Body Mass Index (BMI): kg/m2</li>
+          <li>Sleep Efficiency: Fraction from 0 to 1</li>
+        </ul>
+      </span>
+      <PlotColsGroup
+        fields={[
+          {
+            color: "lightcoral",
+            label: "BMI",
+            column: "BMI",
+            xlabel: "BMI (kg/m2)",
+            ylabel: "Number of records",
+          },
+          {
+            color: "teal",
+            label: "Sleep Efficiency (in fraction)",
+            column: "Sleep_efficiency",
+            xlabel: "Sleep Efficiency",
+            ylabel: "Number of records",
+          },
+        ]}
+        filePath="/data/activity_sleep.parquet"
+        parquetName="activity"
+      />
+
+      <h3 className="text-2xl font-bold">Body Mass Index / Total Sleep Time</h3>
+      <span className="text-sm">
+        <ul className="list-disc ml-5">
+          <li>Body Mass Index (BMI): kg/m2</li>
+          <li>Total Sleep Time: Hours</li>
+        </ul>
+      </span>
+      <PlotColsGroup
+        fields={[
+          {
+            color: "lightcoral",
+            label: "BMI",
+            column: "BMI",
+            xlabel: "BMI (kg/m2)",
+            ylabel: "Number of records",
+          },
+          {
+            color: "darkblue",
+            label: "Total Sleep Time (in hour)",
+            column: "Sleep_duration_h",
+            xlabel: "Total Sleep Time",
+            ylabel: "Number of records",
+          },
+        ]}
+        filePath="/data/activity_sleep.parquet"
+        parquetName="activity"
+      />
+
+
+
+      <h3 className="text-2xl font-bold">Screen Time / Sleep Efficiency (ranges 0 to 1)</h3>
+      <span className="text-sm">
+        <ul className="list-disc ml-5">
+          <li>Screen Time: Hour</li>
+          <li>Sleep Efficiency: Fraction from 0 to 1</li>
+        </ul>
+      </span>
+      <PlotColsGroup
+        fields={[
+          {
+            color: "deepskyblue",
+            label: "Screen Time",
+            column: "Screen_time_h",
+            xlabel: "Screen Time (in hour)",
+            ylabel: "Number of records",
+          },
+          {
+            color: "teal",
+            label: "Sleep Efficiency (in fraction)",
+            column: "Sleep_efficiency",
+            xlabel: "Sleep Efficiency",
+            ylabel: "Number of records",
+          },
+        ]}
+        filePath="/data/activity_sleep.parquet"
+        parquetName="activity"
+      />
+
+
+
+      <h3 className="text-2xl font-bold">Screen Time / Total Sleep Time</h3>
+      <span className="text-sm">
+        <ul className="list-disc ml-5">
+          <li>Screen Time: Hour</li>
+          <li>Total Sleep TIme: Hour</li>
+        </ul>
+      </span>
+      <PlotColsGroup
+        fields={[
+          {
+            color: "deepskyblue",
+            label: "Screen Time",
+            column: "Screen_time_h",
+            xlabel: "Screen Time (in hour)",
+            ylabel: "Number of records",
+          },
+          {
+            color: "darkblue",
+            label: "Total Sleep Time (in hour)",
+            column: "Sleep_duration_h",
+            xlabel: "Total Sleep Time",
+            ylabel: "Number of records",
+          },
+        ]}
+        filePath="/data/activity_sleep.parquet"
+        parquetName="activity"
+      />
 
       {/*
       ***************************
